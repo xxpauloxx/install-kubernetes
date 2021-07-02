8856db2c6712e870c46631b5db8f746ab1704a34
Paulo Roberto <paulo.pinda@gmail.com>
2021-07-02 14:42:34 -0300

    Add script and README.md

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# install-kubernetes
-Simple script for installing Kubernetes on Ubuntu
+# INSTALL KUBERNETES ON UBUNTU
+
+Simple script for installing Kubernetes on Ubuntu using the native package management. My installation at moment is Ubuntu 20.10. 
+
+## GO!
+
+```sh
+$ git clone https://github.com/xxpauloxx/install-kubernetes.git
+$ cd install-kubernetes
+$ ./setup.sh
+```
+
+
+
+#### Bye!
---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "Installing kubectl..."
+
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+echo "Installing minikube..."
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
